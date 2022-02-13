2a5df6c9517bdf962aba43c98ef5c0d6c59ea84e
YerinKim125 <48174646+YerinKim125@users.noreply.github.com>
2022-02-14 02:08:53 +0900

    commit

--- student.js
+++ student.js
@@ -1,1 +1,8 @@
-document.write('hello');
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+var list = document.getElementById('list');
+
+function enter(){
+    document.write('hello');
+}
+
